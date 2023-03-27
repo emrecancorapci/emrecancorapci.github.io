05f0dc5fd0f35686ad33a67ec620028120999e2c
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-03-27 04:27:16 +0300

    project component refactored

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,40 +1,14 @@
 import Card from '../Components/Card';
 import ProjectCard from '../Components/ProjectCard';
 
-const projects = [
-  {
-    title: 'YET ANOTHER BLOG PROJECT BACKEND',
-    technologies: ['LAYERED​ARCHITECTURE', '.NET7', 'ENTITY​FRAMEWORK', 'POSTGRESQL'],
-    source: 'https://github.com/emrecancorapci/YetAnotherBlogProject_Backend',
-  },
-  {
-    title: 'YET ANOTHER BLOG PROJECT FRONTEND',
-    technologies: ['REACT', 'BOOTSTRAP', 'TYPESCRIPT', 'FONT​AWESOME', 'VITE'],
-    source: 'https://github.com/emrecancorapci/YetAnotherBlogProject_Frontend',
-  },
-  {
-    title: 'JOBS API',
-    technologies: ['NODEJS', 'EXPRESSJS', 'MONGODB'],
-    source: 'https://github.com/emrecancorapci/express_jobs-api',
-  },
-  {
-    title: 'BLOG PROJECT',
-    technologies: ['SVELTEKIT', 'BOOTSTRAP', 'VITE'],
-    source: 'https://github.com/emrecancorapci/express_jobs-api',
-  },
-  {
-    title: 'CHESS CONSOLE APP',
-    technologies: ['JUST​C++', 'AND​SOME​STL'],
-    source: 'https://github.com/emrecancorapci/cpp_chess',
-  },
-];
+import projects from '../Data/projects.json';
 
 export default function Projects() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col">
-          <p className="text-center text-white h09">PROJECTS:</p>
+        <div className="col py-2">
+          <p className="text-center text-white h07">PROJECTS:</p>
         </div>
       </div>
       <div className="row">
