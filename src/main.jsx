a8a315ee66f40eac2074a9a0c811f30e72b1af54
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-04-05 19:25:45 +0300

    bootstrap fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as bootstrap from 'bootstrap';
 
 import App from './App';
 import ActivePageProvider from './Contexts/ActiveContext';
 
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './scss/styles.scss';
 
 /**
  * Main component
